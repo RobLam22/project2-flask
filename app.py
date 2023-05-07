0a14ca9263de371bfc63f8869e12ed32353c74e0
rlam22 <robert.lam22@gmail.com>
2023-05-07 11:37:25 +1000

    test home

--- app.py
+++ app.py
@@ -5,14 +5,23 @@ import psycopg2
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    # connection = psycopg2.connect(host=os.getenv("PGHOST"), user=os.getenv("PGUSER"), password=os.getenv("PGPASSWORD"), port=os.getenv("PGPORT"), dbname=os.getenv("PGDATABASE"))
+    def home():
+    # connection = psycopg2.connect(host=os.getenv("PGHOST"), user=os.getenv("pgrender"), password=os.getenv("PGPASSWORD"), port=os.getenv("PGPORT"), dbname=os.getenv("PGDATABASE"))
     connection = psycopg2.connect(os.getenv("DATABASE_URL"))
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM boardgame_board;")
     results = cursor.fetchall()
     connection.close()
-    return f"{results[0]}"
+    return render_template('home.html', home_items = results)
+
+@app.route('/about')
+    def about_page():
+
+@app.route('/signup')
+    def signup_page():
+
+@app.route('/explore')
+    def explore_page():
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
