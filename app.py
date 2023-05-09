0e1fb0fe1006c903a96b26e48c95c03f13a2311f
rlam22 <robert.lam22@gmail.com>
2023-05-09 18:33:23 +1000

    test upload with .env setup

--- app.py
+++ app.py
@@ -7,15 +7,12 @@ import bcrypt
 # app.config['SECRET_KEY'] = my_secret_key
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "My secret key"
 
 @app.route('/')
 def homepage(): 
-    connection = psycopg2.connect(  host=os.getenv("PGHOST", "dpg-ch8fge6si8uhth779pq0-a.oregon-postgres.render.com"), 
-                                    user=os.getenv("pgrender"), 
-                                    password=os.getenv("PGPASSWORD", "YsRbSGWctAQTPL8pX5sOear938QrwlLW"), 
-                                    port=os.getenv("PGPORT", "5432"), 
-                                    dbname=os.getenv("PGDATABASE", "flaskdb_uiod"))
-    # connection = psycopg2.connect(os.getenv("DATABASE_URL"))
+    # connection = psycopg2.connect(host=os.getenv("PGHOST"), user=os.getenv("pgrender"), password=os.getenv("PGPASSWORD"), port=os.getenv("PGPORT"), dbname=os.getenv("PGDATABASE"))
+    connection = psycopg2.connect(os.getenv("DATABASE_URL"))
     # connection = psycopg2.connect(dbname="p2", port=5433, user="postgres", password="roblam")
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM homepage_board;")
@@ -26,11 +23,7 @@ def homepage():
 @app.route('/about')
 def about_page():
     # connection = psycopg2.connect(dbname="p2", port=5433, user="postgres", password="roblam")
-    connection = psycopg2.connect(  host=os.getenv("PGHOST", "dpg-ch8fge6si8uhth779pq0-a.oregon-postgres.render.com"), 
-                                user=os.getenv("pgrender"), 
-                                password=os.getenv("PGPASSWORD", "YsRbSGWctAQTPL8pX5sOear938QrwlLW"), 
-                                port=os.getenv("PGPORT", "5432"), 
-                                dbname=os.getenv("PGDATABASE", "flaskdb_uiod"))
+    connection = psycopg2.connect(os.getenv("DATABASE_URL"))
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM homepage_board;")
     results = cursor.fetchall()
@@ -40,11 +33,7 @@ def about_page():
 @app.route('/explore')
 def explore_page():
     # connection = psycopg2.connect(dbname="p2", port=5433, user="postgres", password="roblam")
-    connection = psycopg2.connect(  host=os.getenv("PGHOST", "dpg-ch8fge6si8uhth779pq0-a.oregon-postgres.render.com"), 
-                                user=os.getenv("pgrender"), 
-                                password=os.getenv("PGPASSWORD", "YsRbSGWctAQTPL8pX5sOear938QrwlLW"), 
-                                port=os.getenv("PGPORT", "5432"), 
-                                dbname=os.getenv("PGDATABASE", "flaskdb_uiod"))
+    connection = psycopg2.connect(os.getenv("DATABASE_URL"))
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM board_list;")
     results = cursor.fetchall()
@@ -60,9 +49,14 @@ def signup_action():
     name = request.form.get('name')
     email = request.form.get('email')
     password = request.form.get('password')
+    hashed_pw = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
 
-    hashed_pw = bcrypt.hashpw(password.encode(), salt).decode()
-    user.add_user(name, email, hashed_pw)
+    # connection = psycopg2.connect(dbname="p2", port=5433, user="postgres", password="roblam")
+    connection = psycopg2.connect(os.getenv("DATABASE_URL"))
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO users (name, email, password_hash, signed_in) VALUES (%s, %s, %s, %s);", [name, email, hashed_pw, False])
+    connection.commit()
+    connection.close()
 
     return redirect("/login")
 
@@ -73,35 +67,70 @@ def login_page():
 @app.route('/login', methods=['POST'])
 def login_action():
     # connection = psycopg2.connect(dbname="p2", port=5433, user="postgres", password="roblam")
-    connection = psycopg2.connect(  host=os.getenv("PGHOST", "dpg-ch8fge6si8uhth779pq0-a.oregon-postgres.render.com"), 
-                                user=os.getenv("pgrender"), 
-                                password=os.getenv("PGPASSWORD", "YsRbSGWctAQTPL8pX5sOear938QrwlLW"), 
-                                port=os.getenv("PGPORT", "5432"), 
-                                dbname=os.getenv("PGDATABASE", "flaskdb_uiod"))
+    connection = psycopg2.connect(os.getenv("DATABASE_URL"))
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM users;")
     results = cursor.fetchall()
-    connection.close()
 
     email = request.form.get('email')
     password = request.form.get('password')
-    hashed_password = user.get_user("WHERE email=%s", [email])["hashed_password"]
-    isValidPassword = bcrypt.checkpw(password.encode(), hashed_password.encode())
-    curr_user = user.get_user("WHERE email=%s", [email])
-    if curr_user:
-        session["user_id"] = curr_user["id"]
-        session["user_name"] = curr_user["name"]
-        if isValidPassword == True:
-            print("WORKS")
-        else:
-            print("YEAH NAH")
-        return redirect('/menu')
+    cursor.execute("SELECT * FROM users WHERE email=%s", [email])
+    user_info = cursor.fetchall()
+    stored_password = user_info[0][3]
+    isValidPassword = bcrypt.checkpw(password.encode(), stored_password.encode())
+    # look into sessions, might not need signed_in column
+    curr_user = user_info[0][2]
+    cursor.execute(f"UPDATE users SET signed_in=true WHERE id={user_info[0][0]};")
+    connection.commit()
+    cursor.execute("SELECT * FROM users WHERE email=%s", [email])
+    results2 = cursor.fetchall()
+    # print(results2[0])
+    session['email'] = email
+    print(session['email'])
+    connection.close()
+    if isValidPassword == True:
+        print("WORKS")
+        return redirect('/')
     else:
-        return render_template("login_error.html")
-    return render_template('login.html')
+        return render_template('/login_error.html')
 
+@app.route('/new_board')
+def new_board():
+    return render_template('/new_board.html')
 
+@app.route('/new_board', methods=['POST'])
+def new_board_action():
+    board_name = request.form.get('board_name')
+    description = request.form.get('description')
+    is_private = request.form.get('access') #returns on if checked
 
+    # connection = psycopg2.connect(dbname="p2", port=5433, user="postgres", password="roblam")
+    connection = psycopg2.connect(os.getenv("DATABASE_URL"))
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE signed_in = True;")
+    user_id = cursor.fetchall()[0][0]
+    if is_private == 'on':
+        cursor.execute("INSERT INTO board_list (board_name, description, is_private, user_id_access, created_by) VALUES (%s, %s, %s, %s, %s);", [board_name, description, True, user_id, user_id])
+    else:
+        cursor.execute("INSERT INTO board_list (board_name, description, is_private, created_by) VALUES (%s, %s, %s, %s);", [board_name, description, False, user_id])
+    cursor.execute(f"SELECT id FROM board_list WHERE board_name = '{board_name}';")
+    board_id = cursor.fetchall()[0][0]
+    cursor.execute(f"CREATE TABLE {board_name} (id SERIAL PRIMARY KEY, description TEXT, hashtag TEXT, image TEXT, user_id BIGINT, board_list_id BIGINT);")
+    connection.commit()
+    connection.close()
+
+    # return redirect('/board_<name>')
+    return redirect('/board_<{board_name}>')
+
+@app.route('/board_<name>')
+def view_board(name):
+    # connection = psycopg2.connect(dbname="p2", port=5433, user="postgres", password="roblam")
+    connection = psycopg2.connect(os.getenv("DATABASE_URL"))
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM {name};")
+    connection.close()
+
+    return render_template('/view_board.html')
 
 if __name__ == '__main__':
     # app.run(debug=True, port=os.getenv("PORT", default=5000))
